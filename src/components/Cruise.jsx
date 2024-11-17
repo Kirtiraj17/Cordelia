e2d3803ebbfd8a1f90515513347ce917dcd31952
Kirtiraj17 <kirtirajg1997@gmail.com>
2024-11-17 20:17:15 +0530

    popover changes

--- src/components/Cruise.jsx
+++ src/components/Cruise.jsx
@@ -35,43 +35,16 @@ const Cruise = ({ itineraries }) => {
             <PopoverPanel
               transition
               anchor="bottom"
-              className="drop-shadow bg-white divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0"
+              className="w-[300px] mt-2 drop-shadow border bg-white text-black divide-y divide-white/5 rounded-xl text-sm/6 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0"
             >
               <div className="p-3">
                 <a
                   className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
                   href="#"
                 >
-                  <p className="font-semibold text-white">Insights</p>
-                  <p className="text-white/50">
+                  <p className="font-semibold">Insights</p>
+                  <p className="">
                     Measure actions your users take
-                  </p>
-                </a>
-                <a
-                  className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
-                  href="#"
-                >
-                  <p className="font-semibold text-white">Automations</p>
-                  <p className="text-white/50">
-                    Create your own targeted content
-                  </p>
-                </a>
-                <a
-                  className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
-                  href="#"
-                >
-                  <p className="font-semibold text-white">Reports</p>
-                  <p className="text-white/50">Keep track of your growth</p>
-                </a>
-              </div>
-              <div className="p-3">
-                <a
-                  className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
-                  href="#"
-                >
-                  <p className="font-semibold text-white">Documentation</p>
-                  <p className="text-white/50">
-                    Start integrating products and tools
                   </p>
                 </a>
               </div>
@@ -98,7 +71,7 @@ const Cruise = ({ itineraries }) => {
             <PopoverPanel
               transition
               anchor="bottom"
-              className="bg-black divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0"
+              className="w-[300px] mt-2 bg-black divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0"
             >
               <div className="p-3">
                 <a
